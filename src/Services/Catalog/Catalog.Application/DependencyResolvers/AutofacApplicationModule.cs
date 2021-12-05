c4a6950cc7c09b7209162c9f17330d3a278ec52a
ilkerogtc <ilkerogtc@gmail.com>
2021-12-05 17:57:51 +0300

    chore(catalog-service): INotificationHandler registered

--- src/Services/Catalog/Catalog.Application/DependencyResolvers/AutofacApplicationModule.cs
+++ src/Services/Catalog/Catalog.Application/DependencyResolvers/AutofacApplicationModule.cs
@@ -19,6 +19,8 @@ namespace Catalog.Application.DependencyResolvers
                 .AsClosedTypesOf(typeof(IRequestHandler<,>));
             builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
                 .AsClosedTypesOf(typeof(IValidator<>));
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .AsClosedTypesOf(typeof(INotificationHandler<>));
             builder
                 .RegisterType<Mediator>()
                 .As<IMediator>()
